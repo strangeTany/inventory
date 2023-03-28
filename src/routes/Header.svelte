32ff5b567bc1480a868e9be77adae0cbb72756ec
Tany Ivshina <strangetany@gmail.com>
2023-03-28 16:51:53 +0300

    Add page for item collection

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,8 +19,8 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/item_collection' ? 'page' : undefined}>
+				<a href="/about">Item Collection</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
---
+++ src/routes/item_collection/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Item Collection</title>
+	<meta name="description" content="Collection of items in stock" />
+</svelte:head>
+
+<div class="text-column">
+	<p>
+		Here will be list of cards with items in stock
+	</p>
+</div>
