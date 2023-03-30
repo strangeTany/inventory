2ee4624d3e8c2ad4b3e7831ea114ffe2402e1e61
Tany Ivshina <strangetany@gmail.com>
2023-03-30 20:01:08 +0300

    Add item collection visual

--- src/routes/item_collection/+page.svelte
+++ src/routes/item_collection/+page.svelte
@@ -1,10 +1,27 @@
+<script lang="ts">
+    import type {Item} from '../item'
+    import ItemCard from './ItemCard.svelte'
+    let items: Item[] = [{name: "Hammer", id: "001", description: "Good hammer", price: 100, amount: 2, vendor: "Vendor1"}, 
+    {name: "Hammer", id: "002", description: "Good hammer, almost fantastic hammer", price: 100, amount: 2, vendor: "Vendor1"},
+    {name: "Hammer", id: "003", description: "Good hammer", price: 100, amount: 2, vendor: "Vendor1"},
+    {name: "Hammer", id: "004", description: "Good hammer", price: 100, amount: 2, vendor: "Vendor1"}];
+</script>
 <svelte:head>
 	<title>Item Collection</title>
 	<meta name="description" content="Collection of items in stock" />
 </svelte:head>
 
-<div class="text-column">
-	<p>
-		Here will be list of cards with items in stock
-	</p>
+<div class="cards_grid">
+	{#each items as item}
+        <ItemCard {item}></ItemCard>
+    {/each}
 </div>
+
+<style>
+    .cards_grid{
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: auto;
+        grid-gap: 1rem;
+    }
+</style>
