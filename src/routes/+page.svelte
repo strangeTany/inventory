6c33429566123d13b8b724c424d3f5660c1ebbe2
sasha-golovin-01 <55413919+sasha-golovin-01@users.noreply.github.com>
2023-04-24 15:26:07 +0300

    home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,33 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture />
-		</span>
+<section class="bgimage" id="home">
+		<h2 class="hero_title" style="color: aliceblue">Work with our inventories!</h2>
+	    <p class="hero_desc">Our capacity is more than you could need!</p>
+</section>
 
-		to our new SvelteKit app "Inventory Management"
-	</h1>
+<section>
+	<button class="use_web" >
+		<a href="/inventory" class="btn btn-primary">Use the website</a></button>
+</section>
+
+<section id="contact">
+	<div class="container mt-3 contactContent">
+		<h1 class="text-center">Contact Our Company if you need a solution!</h1>
+
+		<div class="row mt-4">
+			<div class="col-lg-6">
+
+				<div style="overflow:hidden;resize:none;max-width:100%;width:500px;height:500px;">
+					<div id="my-map-canvas" style="height:100%; width:100%;max-width:100%;">
+						<iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Innopolis,+Республика+Татарстан,+Россия&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
+					</div>
+					<a class="googl-ehtml" rel="nofollow" href="https://www.bootstrapskins.com/themes" id="authorize-maps-data">premium bootstrap themes</a>
+					<style>#my-map-canvas img.text-marker{max-width:none!important;background:none!important;}img{max-width:none}</style></div>
+			</div>
+
+		</div>
+	</div>
 </section>
 
 <style>
@@ -44,4 +63,40 @@
 		top: 0;
 		display: block;
 	}
+
+	.bgimage {
+		height:100vh;
+		background: url("$lib/images/bg.jpeg");
+		background-size:cover;
+		position:relative;
+	}
+	.hero_title {
+		font-size: 4.5rem;
+	}
+	.hero_desc {
+		font-size: 2rem;
+	}
+	.use_web {
+		background: #ffe4dc;
+		color: #dc572f;
+		border: 1px solid #dc572f;
+		padding: 15px 32px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 16px;
+	}
+	.hero-text {
+		text-align: center;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		color: white;
+	}
+	.imageAboutPage {
+		width: 100%;
+	}
+
+
 </style>
